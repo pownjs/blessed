bce8cb6525dbd030121befcfe46e4c5516125341
Petko D. Petkov <pdp@gnucitizen.org>
2019-01-26 10:48:26 +0000

    Refactoring + console.

---
+++ examples/console.js
@@ -0,0 +1,29 @@
+const { screen } = require('../lib/blessed')
+const Quit = require('../lib/auxiliary/quit')
+const Console = require('../lib/auxiliary/console')
+
+const s = screen({
+    title: 'HTTPView'
+})
+
+const q = new Quit()
+const c = new Console({hidden: false})
+
+c.hijackConsole()
+
+s.append(q)
+s.append(c)
+
+s.render()
+
+setInterval(() => {
+    console.log(`${Math.random().toString(32).slice(2)}`)
+}, 1000)
+
+setInterval(() => {
+    console.warn(`${Math.random().toString(32).slice(2)}`)
+}, 2000)
+
+setInterval(() => {
+    console.error(`${Math.random().toString(32).slice(2)}`)
+}, 3000)
--- examples/httpview.js
+++ examples/httpview.js
@@ -1,5 +1,5 @@
 const { screen } = require('../lib/blessed')
-const Quit = require('../lib/quit')
+const Quit = require('../lib/auxiliary/quit')
 const HTTPView = require('../lib/auxiliary/httpview')
 
 const s = screen({
@@ -17,7 +17,6 @@ s.render()
 setInterval(() => {
     h.setMessage(`${Math.random().toString(32).slice(2)}`)
 }, 1000)
-
 
 setInterval(() => {
     h.addTransaction({uri: `http://test/${Math.random().toString(32).slice(2)}`})
